22b79fc74b10aeb0031dffc9450e0ee286cc6d1e
KnifeNose <126018005+KnifeNose@users.noreply.github.com>
2023-05-24 16:43:40 +0100

    Update snake.cpp

--- Version 4/snake.cpp
+++ Version 4/snake.cpp
@@ -10,12 +10,14 @@ Snake::Snake()
 
 void Snake::setup()
 {
+	// Starting position
 	pos[0].first = 10;
 	pos[0].second = 10;
 }
 
 void Snake::update()
 {
+	// Make the tail follow the head of the snake
 	int tempX = pos[0].first;
 	int tempY = pos[0].second;
 	int tempX2, tempY2;
@@ -32,7 +34,8 @@ void Snake::update()
 		tempX = tempX2;
 		tempY = tempY2;
 	}
-
+	
+	// End game if the snakes head hits the tail
 	for (int i = 1; i < this->length; i++)
 	{
 		if ((pos[0].first == pos[i].first) && (pos[0].second == pos[i].second))
@@ -41,6 +44,7 @@ void Snake::update()
 		}
 	}
 	
+	// End game if the snakes head hits the edge of the board 
 	if ((this->pos[0].first <= 0) || (this->pos[0].first >= 20))
 	{
 		this->endGame = true;
