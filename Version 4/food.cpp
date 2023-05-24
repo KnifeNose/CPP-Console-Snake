59690bc819a26a3decbd562e94733e48bf87c64a
KnifeNose <126018005+KnifeNose@users.noreply.github.com>
2023-05-24 16:39:50 +0100

    Update food.cpp

--- Version 4/food.cpp
+++ Version 4/food.cpp
@@ -2,7 +2,6 @@
 #include <random>
 
 Food::Food()
-
 {
 	pos.first = 0;
 	pos.second = 0;
@@ -10,12 +9,15 @@ Food::Food()
 
 void Food::setup()
 {
+	// Random starting position 
 	pos.first = rand() % 20;
 	pos.second = rand() % 20;
 }
 
 void Food::update(std::pair<int, int> snake)
 {
+	// If position is randomly selected where snake is.
+	// Select another random positon
 	if ((snake.first == this->pos.first) && (snake.second == this->pos.second))
 	{
 		pos.first = rand() % 20;
